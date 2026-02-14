d4b84aa209fd8a34a5f2a8d73e643190e03ddcfd
Aditya Singh <90045763+adiityasiingh@users.noreply.github.com>
2026-02-14 15:54:51 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const yesBtn = document.getElementById("yesBtn");
 const noBtn = document.getElementById("noBtn");
 const response = document.getElementById("response");
+const loveImage = document.getElementById("loveImage");
+const cryImage = document.getElementById("cryImage")
 
-yesBtn.addEventListener("click", () => {
-    response.textContent = "Good Choice 😍";
-});
 
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * 200 - 100;
@@ -12,3 +11,38 @@ noBtn.addEventListener("mouseover", () => {
     noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
+noBtn.addEventListener("click", () => {
+    let seconds = 14;
+
+    response.textContent = `Accha silaa diya tune mere pyaar ka...💔
+    Reloading in ${seconds} seconds... apni galti sudhaar lo jaan😚`;
+
+    cryImage.style.display = "block";
+
+    yesBtn.style.display = "none";
+    noBtn.style.display = "none";
+    const countdown = setInterval(() => {
+        seconds--;
+
+        if (seconds > 0) {
+            response.textContent = `AAccha silaa diya tune mere pyaar ka...💔
+    Reloading in ${seconds} seconds... apni galti sudhaar lo jaan😚`;
+        } else {
+            clearInterval(countdown);
+            location.reload();
+        }
+    }, 1000);
+
+
+
+})
+
+yesBtn.addEventListener("click", () => {
+    response.textContent = "Good choice Babygurlll 😘❤️";
+
+    loveImage.style.display = "block";
+
+    // Hide both buttons
+    yesBtn.style.display = "none";
+    noBtn.style.display = "none";
+});
